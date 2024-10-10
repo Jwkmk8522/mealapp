5fd59649d634142427679a998ccfdb4b9d311c4d
jwkmk8522 <jwkmk8522@gmail.com>
2024-10-10 07:32:33 -0700

    change

---
+++ lib/mealapp.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class mealapp extends StatelessWidget {
+  const mealapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
